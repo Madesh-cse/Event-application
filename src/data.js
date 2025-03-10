5bf925bd80b7839f16db06ddc6f563c9e9b033b9
Madesh <madeshdex10@gmail.com>
2025-03-10 21:51:03 +0530

    Add assest

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import event1 from './assets/Teachnical2.jpg'
+import Nonteh from './assets/NonTeh2.jpg'
+import event2 from './assets/Technical1.jpg'
+import event3 from './assets/Technical3.jpg'
+import Nonteh1 from './assets/NonTeh1.jpg'
+import Nonteh2 from './assets/NonTeh3.jpg'
+export const TabsData = {
+
+    Technical:{
+        image:event1,
+        Title:'Presentation BlitZ',
+        Link:'/event/Presentation-BlitZ',
+        image1:event2,
+        Title1:'Code Rescue',
+        Link1 : '/event/Code Rescue',
+        image2:event3,
+        Title2:'WebWhiz',
+        Link2 : '/event/WebWhiz'
+    },
+    NonTechnical:{
+        image:Nonteh,
+        Title:'Cinimatica',
+        Link:'/event/hunt',
+        image1:Nonteh1,
+        Title1:'FunFinity Wars',
+        Link1:'/event/hunt',
+        image2:Nonteh2,
+        Title2:'Shark Tank',
+        Link2:'/event/hunt'
+    }
+
+}
