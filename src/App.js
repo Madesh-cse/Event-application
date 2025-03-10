7de731349d9e8572f3285137ebd923bb33591f43
Madesh <madeshdex10@gmail.com>
2025-03-10 21:52:54 +0530

    update package and router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import PaperPresentation from './Pages/Events/PaperPresntation';
+import CodeRescue from './Pages/Events/CodeRescue';
+import WebWhiz from './Pages/Events/WebWhiz';
+import Cinematica from './Pages/Events/Cinematica';
+import FunFinity from './Pages/Events/FunFinity';
+import SharkTank from './Pages/Events/SharkTank';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='events/Presentation-BlitZ' element={<PaperPresentation/>}/>
+        <Route path='events/CodeRescue' element={<CodeRescue/>}/>
+        <Route path='events/WebWhiz' element={<WebWhiz/>}/>
+        <Route path='events/Cinematica' element={<Cinematica/>}/>
+        <Route path='events/FunFinity-Wars' element={<FunFinity/>}/>
+        <Route path ='events/SharK-Tank' element={<SharkTank/>}/>
+      </Routes>
+  </Router>
+  )
+  
 }
 
 export default App;
